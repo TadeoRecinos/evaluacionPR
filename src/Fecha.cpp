054c6c77bbe280cf012a6904667cd08fabb7661a
TadeoRecinos <josuetadeo45@gmail.com>
2024-03-23 00:27:07 -0600

    Evaluacion

---
+++ src/Fecha.cpp
@@ -0,0 +1,18 @@
+#include "Fecha.h"
+#include <iostream>
+
+Fecha::Fecha(int _dia,int _mes, int _anio)
+{
+    this->dia=_dia;
+    this->mes=_mes;
+    this->anio=_anio;
+}
+Fecha::~Fecha()
+{
+    //dtor
+}
+
+void Fecha::mostrarFecha(){
+     cout << dia << "/" << mes << "/" << anio;
+
+}
